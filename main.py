58ddde1f9f0163b2b3cef83c5aee7735062fc385
SantiMagris01 <santiagomagris1905@gmail.com>
2023-08-08 11:24:33 -0300

    commit

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ model.fit(X_train, y_train)
 
 @app.get("/prediccion/")
 def obtener_prediccion(
-    genero: list = Query(..., description="Lista de géneros", example=["Free to Play", "Indie"]),
+    genero: list = Query(..., description="Lista de géneros", example=["Action", "Indie"]),
     release_year: int = Query(..., description="Año de lanzamiento"),
     developer: str = Query(..., description="Desarrollador")
 ):
